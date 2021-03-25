0a86a6300973d8a6495baa6329630f369789bccb
DESKTOP-3QKRIRT\асус <vish_ig2@mail.ru>
2021-03-25 19:14:26 +0500

    Убрал лишний код в CSS-файле. Доделал алгоритмическую часть алгоритма А*.

--- A_star_algorytm/A_star_main.js
+++ A_star_algorytm/A_star_main.js
@@ -2,47 +2,12 @@
 
 //const buckets = require("./buckets");
 
-//функция для получения случайного числа(будет нужна для лабиринта)
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-//структура точки
-function point(x, y, isWall) {
-    this.x = x;
-    this.y = y;
-    if (isWall !== undefined) {
-        this.isWall = isWall;
-    }
-    else {
-        this.isWall = true;
-    }
-    this.isVisited = false;
-    this.h;//эвр. расстояние до конца 
-    this.g = Infinity;//расстояние от начальной вершины до этой 
-    this.f;//h+g
-    this.parent;//из какой точки попали в эту
-}
-//тестовая карта 9 на 9
-var n = 9;
-//creating map
-let map = new Array(n);
-for (let i = 0; i < n; ++i) {
-    map[i] = new Array(n);
-    for (let j = 0; j < n; ++j) {
-        map[i][j] = new point(i, j, false);
-    }
-}
-//create wall
-for (let i = 0; i < 5; ++i) {
-    map[4][i + 2].isWall = true;
-}
 //create beginning and end
-var beginPoint = map[1][4];
-var endPoint = map[7][4];
+var beginPoint = map[0][4];
+var endPoint = map[8][4];
 //Нахождение эвристического пути
 function distance(somePoint) {
-    let distance = Math.max(Math.abs(endPoint.x - somePoint.x) + Math.abs(endPoint.y - somePoint.y)) * 14;
-    distance += Math.abs(Math.abs(endPoint.x - somePoint.x) - Math.abs(endPoint.y - somePoint.y)) * 10;
+    let distance = Math.abs(endPoint.x - somePoint.x) + Math.abs(endPoint.y - somePoint.y);
     return distance;
 }
 //Забиваем значение эвр.пути длч каждой вершины
@@ -51,9 +16,6 @@ for (let i = 0; i < n; ++i) {
         map[i][j].h = distance(map[i][j]);
     }
 }
-//Инициализируем эти значения для начала и конца
-beginPoint.h=distance(beginPoint);
-endPoint.h=0;
 //Переменная, показывающая существование пути, длина диагонального и ортогонального перехода
 var pathIsExist;
 var ortLength = 10;
@@ -61,48 +23,87 @@ var diagLength = 14;
 //собственно сам алгоритм
 function a_star() {
     //настраиваем функцию сравнения в открытой куче
-    var openList = buckets.Heap(function(a,b){
-        if(a.h+a.g<b.g+b.h)
-        {
+    var openList = buckets.Heap(function (a, b) {
+        if (a.h + a.g < b.g + b.h) {
             return -1;
         }
-        if(a.h+a.g>b.g+b.h){
+        if (a.h + a.g > b.g + b.h) {
             return 1;
         }
         return 0;
     });
-    beginPoint.distFromBeg=0;
+    beginPoint.g = 0;
     openList.add(beginPoint);
-    while (openList != 0) {
-        currentPoint = openList.removeRoot();
-        if (currentPoint.x == endPoint.x && currentPoint.y == endPoint.y) {
+    let cx;
+    let cy;
+    let tentativeScore;
+    while (openList.isEmpty != true) {
+        let currentPoint = openList.removeRoot();
+        if (currentPoint == endPoint) {
             return true;
         }
-        closedList.add()
-        currentPoint.isVisited=true;
-        let cx=currentPoint.x;
-        let cy=currentPoint.y;
-        let tentativeScore;
-        //Здесь нужно перебрать соседние вершины
-        for (let i=-1;i<2;++i)
-        {
-            for (let j = -1;j<2;++j)
-            {
+        currentPoint.isVisited = true;
+        currentPoint.isInOpenList = false;
+        cx = currentPoint.x;
+        cy = currentPoint.y;
+        //Здесь перебор соседниx вершин
+        for (let i = -1; i < 2; ++i) {
+            for (let j = -1; j < 2; ++j) {
                 //проверка на границы, а также являются ли координаты равными current
-                if (cx+i==n || cx+i ==-1 || cy+j==n ||cy+j==0 ||(i==0 && j == 0)){
+                if (cx + i == n || cx + i == -1 || cy + j == n || cy + j == -1 || (i == 0 && j == 0)) {
                     continue;
                 }
-                if ((i+j)%2 == 1){
+                //проверка на стены рядом с текущей клеткой
+                if ((i + j) % 2 == 0) {
+                    if (map[cx][cy + j].isWall === true || map[cx + i][cy].isWall === true)
+                        continue;
+                }
+                if (map[cx + i][cy + j].isWall === true)
+                    continue;
+                //новый кратчайший путь до этой точки через current 
+                if ((i + j) % 2 == 1) {
                     tentativeScore = currentPoint.g + ortLength;
                 }
-                else{
-                    tentativeScore= currentPoint.g + diagLength;
+                else {
+                    tentativeScore = currentPoint.g + diagLength;
                 }
-                if(map[cx+i][cx+j].isVisited == true && tentativeScore >= map[cx+i][cx+j].g)
-                {
-                    /////////////////////////////////////////////////////////////////////////////////////////
+                if (map[cx + i][cy + j].isVisited == true && tentativeScore >= map[cx + i][cy + j].g) {
+                    continue;
                 }
+                //Если у точки появился путь покороче
+                if (map[cx + i][cy + j].isVisited == false && tentativeScore < map[cx + i][cy + j].g) {
+                    map[cx + i][cy + j].parent = currentPoint;
+                    map[cx + i][cy + j].g = tentativeScore;
+                    map[cx + i][cy + j].f = map[cx + i][cy + j].g + map[cx + i][cy + j].h;
+                    //проверка на наличие в открытом списке
+                    if (map[cx + i][cy + j].isInOpenList == false) {
+                        map[cx + i][cy + j].isInOpenList = true;
+                        openList.add(map[cx + i][cy + j]);
+                    }
+                }
+
             }
         }
     }
+    return false;
 }
+
+pathIsExist = a_star();
+function getPath() {
+    if (pathIsExist == false) {
+        console.log("Пути не существует");
+        return;
+    }
+    let currentPoint = endPoint;
+    let fullPath = buckets.Stack();
+
+    while (currentPoint !== beginPoint) {
+        fullPath.push(currentPoint);
+        currentPoint = currentPoint.parent;
+    }
+    let sizeOfStack =fullPath.size();
+    for(let i = 0;i<sizeOfStack;++i){
+        console.log(fullPath.peek().x, fullPath.pop().y);
+    }
+}
+getPath();
--- A_star_algorytm/maze_generation.js
+++ A_star_algorytm/maze_generation.js
@@ -5,15 +5,40 @@
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-
-
-// let a = buckets.Heap();
-// a.add(7);
-// a.add(2);
-// let b = a.toArray();
-// a.add(99);
-// a.forEach(function (e) {
-//     alert(e);
-// })
+//структура точки
+function point(x, y, isWall) {
+    this.x = x;
+    this.y = y;
+    if (isWall !== undefined) {
+        this.isWall = isWall;
+    }
+    else {
+        this.isWall = true;
+    }
+    this.isVisited = false;
+    this.h;//эвр. расстояние до конца 
+    this.g = Infinity;//расстояние от начальной вершины до этой 
+    this.f;//h+g
+    this.parent;//из какой точки попали в эту
+    this.isInOpenList = false;//проверка на наличие этой точки в открытом списке
+}
+//тестовая карта 9 на 9
+var n = 9;
+//creating map
+let map = new Array(n);
+for (let i = 0; i < n; ++i) {
+    map[i] = new Array(n);
+    for (let j = 0; j < n; ++j) {
+        map[i][j] = new point(i, j, false);
+    }
+}
+//create wall
+for (let i = 0; i < 8; ++i) {
+    map[2][i + 1].isWall = true;
+}
+for (let i = 0; i < 7; ++i) {
+    map[6][i].isWall = true;
+}
+map[5][6].isWall = true;
 
 
